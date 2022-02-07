a1e7ff2bb371a4f2536f1893bf6c43c9800eaefc
Maedeh <yazdian.oul@gmail.com>
2022-02-07 12:07:21 +0330

    declare the Employee class

--- src/main/kotlin/Q2.kt
+++ src/main/kotlin/Q2.kt
@@ -1,3 +1,10 @@
 fun main( ){
 
 }
+
+abstract class Employee(val name: String, val age: Int, val hourRate: Double){
+    abstract fun salary()
+    override fun toString(): String {
+        return "name: $name age: $age hourRate: $hourRate"
+    }
+}
