e55a5593b25f9bca7fcf3fb5c9258931484c1e67
Maedeh <yazdian.oul@gmail.com>
2022-02-08 17:09:22 +0330

    Q4: test the method in the main

--- src/main/kotlin/Q4.kt
+++ src/main/kotlin/Q4.kt
@@ -1,8 +1,13 @@
 fun main(){
+    val arrayListInt = arrayListOf(55, 60, 65, 70)
+    arrayListInt.myPrint()
 
+    val arrayListString = arrayListOf("Hello", "world", "!")
+    arrayListString.myPrint()
 }
 
 fun <T> ArrayList<T>.myPrint(){
+    println()
     for (i in 0 until this.size){
         println("$i: ${this[i]}")
     }
