2f47611abb4f0fbe270b48f7c07ec907939d4182
Maedeh <yazdian.oul@gmail.com>
2022-02-09 10:33:21 +0330

    Q5: declare the extension generic function

--- src/main/kotlin/Q5.kt
+++ src/main/kotlin/Q5.kt
@@ -1,3 +1,14 @@
 fun main(){
 
 }
+
+fun <T> List<T>.sliceList(num: Int){
+    val listOut = mutableListOf<MutableList<T>>()
+    for ( i in 0 until this.size){
+        if (i % (num+1) == 0){
+            listOut.add(mutableListOf(this[i]))
+        }
+        else    listOut.last().add(this[i])
+    }
+    println(listOut)
+}
