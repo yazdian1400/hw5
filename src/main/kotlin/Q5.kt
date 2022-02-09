1a89ac479e88fe55cb40cc8022d6f51e62c8f7a0
Maedeh <yazdian.oul@gmail.com>
2022-02-09 10:42:02 +0330

    Q5: rename the function to sliceWith

--- src/main/kotlin/Q5.kt
+++ src/main/kotlin/Q5.kt
@@ -1,15 +1,15 @@
 fun main(){
     val listInt = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    listInt.sliceList(2)
+    listInt.sliceWith(2)
 
     val listChar = listOf('a', 'b', 'c', 'd', 'e', 'f', 'g')
-    listChar.sliceList(3)
+    listChar.sliceWith(3)
 
     val listEmpty = listOf<String>()
-    listEmpty.sliceList(4)
+    listEmpty.sliceWith(4)
 }
 
-fun <T> List<T>.sliceList(num: Int){
+fun <T> List<T>.sliceWith(num: Int){
     val listOut = mutableListOf<MutableList<T>>()
     for ( i in 0 until this.size){
         if (i % (num+1) == 0){
