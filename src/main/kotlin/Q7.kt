6480e6b4513e1b3ce2f0c2d26d9cfb44d3be9c0d
Maedeh <yazdian.oul@gmail.com>
2022-02-09 23:04:56 +0330

    Q7: define class binaryToDecimal and test it in the main

--- src/main/kotlin/Q7.kt
+++ src/main/kotlin/Q7.kt
@@ -7,6 +7,9 @@ fun main(){
 
     println("Enter a binary number:")
     val strBinary = readLine()
+    strBinary?.let{
+        println(binaryToDecimal(it))
+    }
 }
 
 fun decimalToBinary(strDec: String): String{
@@ -27,4 +30,23 @@ fun decimalToBinary(strDec: String): String{
     listBinStr = listBin.map { e -> e.toString() }
     val strOut = listBinStr.reversed().reduce{a,b -> a + b}
     return strOut
+}
+
+fun binaryToDecimal(strBin: String): String{
+    var numDec = 0
+    val strReverse = strBin.reversed()
+    for (i in 0 until strReverse.length){
+        if (strReverse[i] == '1')   numDec += pow2(i)
+        else if (strReverse[i] == '0')  numDec += 0
+        else{
+            println("You didn't enter a binary number!")
+            return ""
+        }
+    }
+    return numDec.toString()
+}
+fun pow2 (num: Int): Int{
+    var result = 1
+    for (i in 0 until num)  result *= 2
+    return result
 }
