2e84940b6891ca6ac6d7dd01b34cc6149d6022cc
Maedeh <yazdian.oul@gmail.com>
2022-02-09 23:24:13 +0330

    Q1: define class Van and SportCar and edit PrivateTransportationVehicle

--- src/main/kotlin/Q1.kt
+++ src/main/kotlin/Q1.kt
@@ -22,8 +22,15 @@ interface ServiceTransportationVehicle{
     }
 }
 interface PrivateTransportationVehicle{
-    fun maxSpeed()
-    fun maxCapacity()
+    var maxSpeed : Double
+    var maxCapacity : Double
+
+    fun maxSpeed(): Double{
+        return maxSpeed
+    }
+    fun maxCapacity(): Double{
+        return maxCapacity
+    }
 }
 class Taxi(override var maxSpeed: Double, override var maxCapacity: Double, val numberPlate: Int, val color: Color ): PublicTransportationVehicle{
 }
@@ -33,7 +40,10 @@ class Ambulance(override var maxSpeed: Double, override var maxCapacity: Double,
 }
 class Truck(override var maxSpeed: Double, override var maxCapacity: Double, val loadCapacity: Int): ServiceTransportationVehicle{
 }
-
+class Van(override var maxSpeed: Double, override var maxCapacity: Double, val grade: Int): PrivateTransportationVehicle{
+}
+class SportCar(override var maxSpeed: Double, override var maxCapacity: Double, val color: Color): PrivateTransportationVehicle{
+}
 enum class Color{
     Yellow,
     Blue,
