1cf0c0524a11dbc543cb692466c5268edd63fe6f
Maedeh <yazdian.oul@gmail.com>
2022-02-09 11:56:02 +0330

    Q1: define taxi class and Color class

--- src/main/kotlin/Q1.kt
+++ src/main/kotlin/Q1.kt
@@ -11,3 +11,13 @@ interface PrivateTransportationVehicle{
     fun maxSpeed()
     fun maxCapacity()
 }
+class Taxi(val numberPlate: Int, val color: Color){
+    var direction = ""
+}
+enum class Color{
+    Yellow,
+    Blue,
+    White,
+    Red,
+    Black
+}
