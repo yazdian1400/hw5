0249d3e55cbf4ddc4179002e1a7a6655897f2215
Maedeh <yazdian.oul@gmail.com>
2022-02-09 10:40:54 +0330

    Q5: test the function in the main

--- src/main/kotlin/Q5.kt
+++ src/main/kotlin/Q5.kt
@@ -1,5 +1,12 @@
 fun main(){
+    val listInt = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    listInt.sliceList(2)
 
+    val listChar = listOf('a', 'b', 'c', 'd', 'e', 'f', 'g')
+    listChar.sliceList(3)
+
+    val listEmpty = listOf<String>()
+    listEmpty.sliceList(4)
 }
 
 fun <T> List<T>.sliceList(num: Int){
